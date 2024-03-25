307c0d170b08a922dfaf92d869bf77c1ac75745f
yukoDotMoe <zgamegaming@gmail.com>
2024-03-26 00:02:52 +0700

    updates carbon

--- app/Http/Controllers/UtilsController.php
+++ app/Http/Controllers/UtilsController.php
@@ -484,7 +484,8 @@ class UtilsController extends Controller
         $rating->product = $request->product;
         $rating->save();
 
-        UtilsController::systemDiscordNotify('User: ' . Auth::user()->username . ' | Đánh giá: ' . $request->stars . ' ⭐ | Với lời nhắn: ' . $request->message . ' | ' . $request->product == 'all' ? 'User đánh giá chung.' : 'Trong sản phẩm: ' . $request->product);
+        $belongTo = $request->product == 'all' ? 'User đánh giá chung.' : 'Trong sản phẩm: ' . $request->product;
+        UtilsController::systemDiscordNotify('User: ' . Auth::user()->username . ' | Đánh giá: ' . $request->stars . ' ⭐ | Với lời nhắn: ' . $request->message . ' | ' . $belongTo);
 
         return response()->json([
             'status' => 1,
